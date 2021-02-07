bff8f97d847696d7400e4b3a295e85b3a88bf609
Haloger7 <54002440+Haloger7@users.noreply.github.com>
2021-02-07 16:00:50 +0300

    Update Ah.ts

--- src/Util/Ah.ts
+++ src/Util/Ah.ts
@@ -1,4 +1,4 @@
-module.exports = (abs: number, max: number) => { 
-if(abs > max) throw new Error('Enter the correct absolute and maximum humidity.'); 
-return (abs/max) * 100 + '%' 
+module.exports = (abs, max) => { 
+if(abs > max) return console.error('Enter the correct absolute and maximum humidity.'); 
+else (abs/max) * 100 + '%'
 }
